b88727e3c8686f443ebf65fb6ec40365883c3019
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MacBook-Pro.local>
2021-10-22 14:08:05 -0500

    solid black square from anchor position

--- sketch02.js
+++ sketch02.js
@@ -8,6 +8,16 @@ const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    const x = 0;
+    const y = 0;
+    const w = width * 0.30;
+    const h = height * 0.30;
+    context.fillStyle = 'black';
+    context.beginPath();
+    context.rect(x, y, w, h,);
+    context.fill();
+    
   };
 };
 
